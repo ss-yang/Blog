11e502dbcc49c14e738042bb27b156abad7f50b9
syang <dcc1@qq.com>
2017-05-20 00:01:50 +0800

    Comment类和comment layout的简单实现。

---
+++ app/src/main/java/com/answer/blog/Data/Comment.java
@@ -0,0 +1,12 @@
+package com.answer.blog.Data;
+
+/**
+ * Created by Answer on 2017/5/18.
+ */
+
+public class Comment {
+    private String id;
+    private String author;
+    private String content;
+    private String date;
+}
